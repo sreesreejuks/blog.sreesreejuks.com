9fe436048d6974f5485b1c03d43df12b6652aeb0
sreesreejuks <sreesreejume@gmail.com>
2025-02-01 20:10:43 +0530

    t 	modified:   404.html 	modified:   about/index.html 	modified:   app.min.js 	modified:   archives/index.html 	modified:   assets/css/jekyll-theme-chirpy.css.map 	modified:   assets/js/data/search.json 	modified:   assets/js/data/swconf.js 	modified:   assets/js/dist/categories.min.js 	modified:   assets/js/dist/commons.min.js 	modified:   assets/js/dist/home.min.js 	modified:   assets/js/dist/misc.min.js 	modified:   assets/js/dist/page.min.js 	modified:   assets/js/dist/post.min.js 	modified:   assets/js/dist/theme.min.js 	new file:   categories/blogging/index.html 	new file:   categories/demo/index.html 	modified:   categories/index.html 	new file:   categories/tutorial/index.html 	modified:   categories/tutorials/index.html 	new file:   customize-the-favicon/index.html 	modified:   feed.xml 	new file:   getting-started/index.html 	modified:   how-to-setup-portainer/index.html 	modified:   index.html 	modified:   sitemap.xml 	modified:   tags/blogging/index.html 	new file:   tags/favicon/index.html 	new file:   tags/getting-started/index.html 	modified:   tags/index.html 	modified:   tags/jekyll/index.html 	modified:   tags/static-site-generator/index.html 	modified:   tags/tutorial/index.html 	new file:   tags/typography/index.html 	new file:   tags/writing/index.html 	new file:   text-and-typography/index.html 	new file:   write-a-new-post/index.html

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1738416034',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1738420740',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
