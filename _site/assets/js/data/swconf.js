0857b41984b3af56fa4cc87251834d4aacb6594a
Sreeju-WSL <tech.sreejuks@gmail.com>
2025-04-02 20:43:14 +0530

    Automated deploy: 2025-04-02

--- _site/assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf = { cacheName: 'chirpy-1743606182',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
