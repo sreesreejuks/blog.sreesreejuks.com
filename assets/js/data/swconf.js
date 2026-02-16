8cdf190a732376bb0cdf014a55485730c0d241d5
Sreeju KS | Terminal <sreesreejuks@gmail.com>
2026-02-16 20:50:00 +0530

    New post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771254843',resources: [
+    cacheName: 'chirpy-1771255124',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
